835b920ace932e71c86c49d7d3d09707b6121a02
toqoboro <pavchekin@mail.ru>
2023-05-26 12:42:56 +0300

    domik

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/toqoboro/frontend-project-44/workflows/hexlet-check/badge.svg)](https://github.com/toqoboro/frontend-project-44/actions)
+[![Maintainability](https://api.codeclimate.com/v1/badges/bb7eb7ed78c81b68dee0/maintainability)](https://codeclimate.com/github/toqoboro/frontend-project-44/maintainability)
--- src/cli.js
+++ src/cli.js
@@ -1,8 +1,9 @@
 import readlineSync from 'readline-sync';
 
-export const greet = () => {
-    // Приветствие
-    console.log('Welcome to the Brain-Games!');
-    const userName = readlineSync.question('What is your name? ');
-    console.log(`Hello ${userName}!`);
-}
+const greet = () => {
+  // Приветствие
+  console.log('Welcome to the Brain-Games!');
+  const userName = readlineSync.question('What is your name? ');
+  console.log(`Hello ${userName}!`);
+};
+export default greet;
