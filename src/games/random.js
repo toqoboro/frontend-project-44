1c58ecb7b25fd9f4a184d10f79ed8109fb5edd1c
Pavel <pavchekin@mail.ru>
2023-05-26 17:09:16 +0300

    forsaj 10

--- src/games/random.js
+++ src/games/random.js
@@ -1,6 +1,6 @@
 function getRandomNum(min, max) {
-    const random = Math.floor(Math.random() * (max - min + 1) + min);
-    return random;
-  }
-  
-  export default getRandomNum;
+  const random = Math.floor(Math.random() * (max - min + 1) + min);
+  return random;
+}
+
+export default getRandomNum;
