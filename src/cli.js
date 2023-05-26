0b926fa1c2f964dcf99febabe30b7daba7b026ff
Pavel <pavchekin@mail.ru>
2023-05-26 17:14:22 +0300

    poslednuizaezd

--- src/cli.js
+++ src/cli.js
@@ -3,7 +3,7 @@ import readlineSync from 'readline-sync';
 const greet = () => {
   // Приветствие
   console.log('Welcome to the Brain Games!');
-  const userName = readlineSync.question('What is your name? ');
+  const userName = readlineSync.question('May I have your name?');
   console.log(`Hello ${userName}!`);
 };
 export default greet;
